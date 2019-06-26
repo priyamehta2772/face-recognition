be32e51f55309584b4776a2d3169e45fbb957d7d
MonicaSai7 <monica.sai7@gmail.com>
2019-06-26 23:04:17 +0530

    Use different combinations of train images

--- face_recognition_dlib_KNN.py
+++ face_recognition_dlib_KNN.py
@@ -4,7 +4,17 @@ Created on Tue Jun 25 11:52:04 2019
 
 @author: LENOVO
 """
-
+"""
+    Accuracy               Train images combination
+88.88888888888889 -> [4, 1, 3, 0, 2, 5, 9, 8, 7, 10, 6]
+57.77777777777778 -> [3, 4, 5, 1, 2, 6, 7, 8, 10, 0, 9]
+93.33333333333333 -> [0, 3, 7, 1, 10, 9, 6, 4, 8, 2, 5]
+93.33333333333333 -> [2, 0, 8, 7, 10, 4, 9, 1, 5, 6, 3]
+55.55555555555556 -> [8, 0, 4, 1, 7, 10, 2, 5, 9, 6, 3]
+62.22222222222222 -> [4, 7, 1, 5, 10, 0, 8, 2, 3, 6, 9]
+91.11111111111111 -> [0, 9, 6, 8, 1, 5, 4, 10, 3, 7, 2]
+62.22222222222222 -> [3, 10, 4, 8, 0, 7, 5, 2, 9, 1, 6]
+"""
 import os
 import dlib
 import cv2
@@ -61,7 +71,10 @@ Test = np.zeros((no_of_classes * (images_per_class - split), no_of_landmarks))
 train = 0
 test = 0
 rep_landmarks = list()
-images = []
+#indices = list(range(11))
+#random.shuffle(indices)
+
+indices = [0, 3, 7, 1, 10, 9, 6, 4, 8, 2, 5]
 for subject in range(1, no_of_classes + 1):
     
     subject_landmarks = []
@@ -71,8 +84,7 @@ for subject in range(1, no_of_classes + 1):
         subject = str(subject)
         
     images_list = [x for x in file_list if re.search("^.*" + subject + ".*pgm$", x)]
-    random.shuffle(images_list)
-    images.append(images_list)
+    images_list = [images_list[i] for i in indices]
 
     for image_name in images_list:
         image_path = dataset_path + "\\" + image_name
@@ -102,7 +114,7 @@ img_count = 0
 
 for test_image_distances in Test:
     img_count += 1
-    Train_diff = np.zeros((no_of_classes * split, no_of_landmarks)) # Matrix of differences of distances
+    Train_diff = np.zeros((no_of_classes * split, no_of_landmarks))
         
     for index in range(len(Train)):
         Train_diff[index,:] = list(map(abs, np.subtract(Train[index], np.asarray(test_image_distances))))
@@ -114,5 +126,6 @@ for test_image_distances in Test:
     if int(Train_diff_means.index(min(Train_diff_means)) // split) == int((img_count - 1) // (images_per_class - split)):
         count += 1
             
-print(count*100/img_count, "->", split)
+print(count*100/img_count, "->", indices)
+
 
--- landmarks_experiment.py
+++ landmarks_experiment.py
@@ -4,16 +4,27 @@ Created on Mon Jun 24 12:00:00 2019
 
 @author: LENOVO
 """
-
-
+'''
+    Accuracy            Train images combination
+28.88888888888889 -> [2, 3, 1, 9, 5, 6, 0, 8, 7, 10, 4]
+35.55555555555556 -> [3, 4, 5, 10, 6, 8, 9, 7, 2, 1, 0]
+26.666666666666668 -> [8, 9, 1, 0, 10, 6, 5, 4, 7, 3, 2]
+42.22222222222222 -> [9, 2, 3, 10, 5, 6, 0, 7, 8, 4, 1]
+40.0 -> [10, 0, 5, 3, 6, 8, 9, 2, 1, 4, 7]
+26.666666666666668 -> [5, 2, 10, 1, 7, 6, 4, 3, 0, 9, 8]
+31.11111111111111 -> [5, 8, 1, 0, 3, 6, 2, 10, 9, 4, 7]
+'''
+import random
 import os
 import dlib
 import cv2
 from imutils import face_utils
 import re
-predictor_path = "C:\Anaconda3\shape_predictor_68_face_landmarks.dat"
-faces_folder_path = "E:\Talentsprint_WE\FaceExpressionRecognitionUsingCNN\\faces"
-new_folder_path = "E:\Talentsprint_WE\detected_faces\\"
+import numpy as np
+import math
+
+def euclidean_distance(x, y):
+    return math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)]))
 
 def atoi(text):
     if text.isdigit():
@@ -27,91 +38,73 @@ def natural_keys(text):
        l.append(atoi(c))     
     return(l)    
 
-file_list=os.listdir(faces_folder_path)
-file_list = [x for x in file_list if re.search("^.*pgm$", x)]
-file_list.sort(key=natural_keys)
-
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(predictor_path)
-
-FACIAL_LANDMARKS_IDXS = OrderedDict([
-	("mouth", (48, 68)),
-	("right_eyebrow", (17, 22)),
-	("left_eyebrow", (22, 27)),
-	("right_eye", (36, 42)),
-	("left_eye", (42, 48)),
-	("nose", (27, 35)),
-	("jaw", (0, 17))
-])
-
-rep_landmarks = list()
-for subject in range(1, 16):
-    
-    subject_landmarks = []
+def get_subject_name(subject):
     if subject < 10:
         subject = "0" + str(subject)
     else:
         subject = str(subject)
-    images_list = [x for x in file_list if re.search("^.*" + subject + ".*pgm$", x)]
-    subject_path = new_folder_path + str(subject) + "\\"
-    if not os.path.exists(subject_path):
-        os.makedirs(subject_path)
+    return subject
 
-    for image_name in images_list[:8]:
+def get_subject_images(subject, file_list):
+    return [x for x in file_list if re.search("^.*" + subject + ".*pgm$", x)]
+
+predictor_path = "C:\Anaconda3\shape_predictor_68_face_landmarks.dat"
+dataset_path = "E:\Talentsprint_WE\FaceExpressionRecognitionUsingCNN\\faces"
+
+file_list = os.listdir(dataset_path)
+file_list = [x for x in file_list if re.search("^.*pgm$", x)]
+file_list.sort(key = natural_keys)
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(predictor_path)
+
+#indices = list(range(11))
+#random.shuffle(indices)
+indices = [9, 2, 3, 10, 5, 6, 0, 7, 8, 4, 1]
+
+split = 8
+Train = np.zeros((15 * split, 2))
+Test = np.zeros((15 * (11 - split), 2))
+train = 0
+test = 0
+for subject in range(1, 16):
     
-        image_path = faces_folder_path + "\\" + image_name
+    subject = get_subject_name(subject)
+    subject_landmarks = []
+    images_list = get_subject_images(subject, file_list)
+    images_list = [images_list[i] for i in indices]
+    for image_name in images_list:
+    
+        image_path = dataset_path + "\\" + image_name
         image = cv2.imread(image_path,-1)
-        #print(image_path)
-        faces = detector(image, 1)
-        for i, face in enumerate(faces):
+        faces_in_image = detector(image, 1)
+        
+        for i, face in enumerate(faces_in_image):
             shape = predictor(image, face)
-            (x, y, w, h) = face_utils.rect_to_bb(face)
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
             shape = face_utils.shape_to_np(shape)
-            for (x, y) in shape:
-                cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
                 
         representative_landmark = [ sum([point[0] for point in shape]) / 68, sum([point[1] for point in shape]) / 68]
-        subject_landmarks.append(representative_landmark)
-        #cv2.imwrite(subject_path + "\\" + image_name, image)
-        #cv2.imshow(image_name, image)
-        #cv2.waitKey(0)
-    rep_landmarks.append(subject_landmarks)
-
-test_image = "E:\Talentsprint_WE\FaceExpressionRecognitionUsingCNN\faces\subject01.wink.pgm"
-faces = detector(image, 1)
-for i, face in enumerate(faces):
-    test_shape = predictor(image, face)
-    (x, y, w, h) = face_utils.rect_to_bb(face)
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    test_shape = face_utils.shape_to_np(test_shape)
-    for (x, y) in test_shape:
-        cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
-test_landmarks = [ sum([point[0] for point in test_shape]) / 68, sum([point[1] for point in test_shape]) / 68]
-
-import math
-def euclidean_distance(x, y):
-    return math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)]))
-
-distances = []
-for person in rep_landmarks:
-    print("Person:",person)
-    train_test_distances = []
-    for train_images_landmarks in person:
-        print("TIL:",train_images_landmarks)
-        dist = euclidean_distance(train_images_landmarks, test_landmarks)
-        train_test_distances.append(dist)
-    print("TTD:",train_test_distances)
-    distances.append([rep_landmarks.index(person), min(train_test_distances)])
-    
-print("Class:", distances)
         
- '''       
-import numpy as np  
-image_batch = np.expand_dims(image, axis=0)
-import keras
-from keras.applications import resnet50
-resnet_model = resnet50.ResNet50(weights = 'imagenet')'''
+        if images_list.index(image_name) < split:
+            Train[train,:] = representative_landmark
+            train += 1
+        else:
+            Test[test,:] = representative_landmark
+            test += 1
+
+count = 0
+img_count = 0
+for test_landmark in Test:
+    img_count += 1
+    distances = []
+    for subject_landmark in Train:
+        dist = euclidean_distance(subject_landmark, test_landmark)
+        distances.append(dist)
     
+    if int(img_count//3) == int(distances.index(min(distances)) // split):
+        count +=  1
+        
+print(count*100/img_count, "->", indices) 
+
     
     
