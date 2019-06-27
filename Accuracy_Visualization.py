a7e6150b5e96806a19d8df22734f9699826e564c
MonicaSai7 <monica.sai7@gmail.com>
2019-06-27 12:53:47 +0530

    jupyter notebooks of Landmark Extraction and Approach_1

--- Accuracy_Visualization.py
+++ Accuracy_Visualization.py
@@ -6,14 +6,11 @@ Created on Wed Jun 26 22:26:47 2019
 """
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
 
-def visualize(accuracy, labels):
+def visualize(accuracy):
     df = pd.DataFrame({'Train Images':list(accuracy.keys()),'Accuracy':list(accuracy.values())})
-    plot = sns.barplot(x = "Train Images",y = "Accuracy",data = df ,hue_order = labels)
-    plt.figure(figsize=(15,16)) 
-    for item in plot.get_xticklabels():
-        item.set_rotation(60)
+    sns.barplot(x = "Train Images",y = "Accuracy",data = df)
+
 
 
 #Visualizing Train images combination for Approach 1
@@ -26,25 +23,38 @@ def visualize(accuracy, labels):
 40.0 -> [10, 0, 5, 3, 6, 8, 9, 2, 1, 4, 7]
 31.11111111111111 -> [5, 8, 1, 0, 3, 6, 2, 10, 9, 4, 7]
 '''
-accuracy = {"[2,3,1,9,5,6,0,8]":28.88, "[3, 4, 5, 10, 6, 8, 9, 7]":35.55, "[8, 9, 1, 0, 10, 6, 5, 4]":26.66, 
-            "[9, 2, 3, 10, 5, 6, 0, 7]":42.22, "[10, 0, 5, 3, 6, 8, 9, 2]":40.0, "[5, 8, 1, 0, 3, 6, 2, 10]":31.11}
-visualize(accuracy, labels)
+accuracy = {"Set 1":28.88, "Set 2":35.55, "Set 3":26.66, "Set 4":42.22, "Set 5":40.0, "Set 6":31.11}
+visualize(accuracy)
 
 # Visualizing Train images combination for Approach 2
 """
     Accuracy               Train images combination
 88.88888888888889 -> [4, 1, 3, 0, 2, 5, 9, 8, 7, 10, 6]
-57.77777777777778 -> [3, 4, 5, 1, 2, 6, 7, 8, 10, 0, 9]
-93.33333333333333 -> [0, 3, 7, 1, 10, 9, 6, 4, 8, 2, 5]
+57.77777777777778 -> [3, 4, 5, 1, 2, 6, 7, 8, 10, 0, 9]  
+93.33333333333333 -> [0, 3, 7, 1, 10, 9, 6, 4, 8, 2, 5]   10,9,7,4,1,0
 93.33333333333333 -> [2, 0, 8, 7, 10, 4, 9, 1, 5, 6, 3]
 55.55555555555556 -> [8, 0, 4, 1, 7, 10, 2, 5, 9, 6, 3]
 62.22222222222222 -> [4, 7, 1, 5, 10, 0, 8, 2, 3, 6, 9]
 91.11111111111111 -> [0, 9, 6, 8, 1, 5, 4, 10, 3, 7, 2]
 62.22222222222222 -> [3, 10, 4, 8, 0, 7, 5, 2, 9, 1, 6]
 """
-accuracy = {0:88.88, 1:57.77, 2:93.33, 3:93.33, 4:55.55, 5:62.22, 6:91.11, 7:62.22}
+accuracy = {"Set 1":88.88, "Set 2":57.77, "Set 3":93.33, "Set 4":93.33, "Set 5":55.55, "Set 6":62.22, "Set 7":91.11, "Set 8":62.22}
 visualize(accuracy)
 
+# Visualizing Train images combination for Approach 3
+'''
+    Accuracy         Image Order
+93.3333333333 -> [4, 1, 3, 0, 2, 5, 9, 8, 7, 10, 6]
+60.0 -> [3, 4, 5, 1, 2, 6, 7, 8, 10, 0, 9]
+93.33333333333333 -> [0, 3, 7, 1, 10, 9, 6, 4, 8, 2, 5]
+95.5555555556 -> [2, 0, 8, 7, 10, 4, 9, 1, 5, 6, 3]
+62.2222222222 -> [8, 0, 4, 1, 7, 10, 2, 5, 9, 6, 3]
+66.6666666667 -> [4, 7, 1, 5, 10, 0, 8, 2, 3, 6, 9]
+91.1111111111 -> [0, 9, 6, 8, 1, 5, 4, 10, 3, 7, 2]
+68.8888888889 -> [3, 10, 4, 8, 0, 7, 5, 2, 9, 1, 6]
+'''
+accuracy = {"Set 1":93.33, "Set 2":60.0, "Set 3":93.33, "Set 4":95.55, "Set 5":62.22, "Set 6":66.66, "Set 7":91.11, "Set 8":68.88}
+visualize(accuracy)
 
 
 
--- svm.py
+++ svm.py
@@ -6,6 +6,17 @@ Created on Tue Jun 25 15:39:33 2019
 
 svm classifier from sklearn
 """
+'''
+    Accuracy         Image Order
+93.3333333333 -> [4, 1, 3, 0, 2, 5, 9, 8, 7, 10, 6]
+60.0 -> [3, 4, 5, 1, 2, 6, 7, 8, 10, 0, 9]
+93.33333333333333 -> [0, 3, 7, 1, 10, 9, 6, 4, 8, 2, 5]
+95.5555555556 -> [2, 0, 8, 7, 10, 4, 9, 1, 5, 6, 3]
+62.2222222222 -> [8, 0, 4, 1, 7, 10, 2, 5, 9, 6, 3]
+66.6666666667 -> [4, 7, 1, 5, 10, 0, 8, 2, 3, 6, 9]
+91.1111111111 -> [0, 9, 6, 8, 1, 5, 4, 10, 3, 7, 2]
+68.8888888889 -> [3, 10, 4, 8, 0, 7, 5, 2, 9, 1, 6]
+'''
 
 import pandas as pd
 import os
@@ -17,6 +28,7 @@ from collections import OrderedDict
 import numpy as np
 import math
 from sklearn import svm
+import random
 
 def atoi(text):
     if text.isdigit():
@@ -33,8 +45,8 @@ def natural_keys(text):
 def euclidean_distance(x, y):
     return math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)]))
 
-predictor_path = "C:\\Users\\priya\\Anaconda3\\shape_predictor_68_face_landmarks.dat"
-faces_folder_path = "C:\\Users\\priya\\Anaconda3\\YALE\\faces"
+predictor_path = "shape_predictor_68_face_landmarks.dat"
+faces_folder_path = "faces"
    
 file_list=os.listdir(faces_folder_path)
 file_list = [x for x in file_list if re.search("^.*pgm$", x)]
@@ -54,11 +66,19 @@ FACIAL_LANDMARKS_IDXS = OrderedDict([
 ])
 
 split = 8
-Train=np.zeros((15 * split,68)) # Matrix of distances
-z = 0
+Train = np.zeros((15 * split,68)) 
+Test = np.zeros((15 * (11-split),68)) # Matrix of distances
 
-target = np.zeros((15 * split,1))
+train = 0
+test = 0
+train_target = np.zeros((15 * split,1))
+test_target = np.zeros((15 * (11-split),1))
 target_index = 0
+test_target_index = 0
+
+#indices = list(range(11))
+#random.shuffle(indices)
+indices = [2, 0, 8, 7, 10, 4, 9, 1, 5, 6, 3]
 
 rep_landmarks = list()
 for subject in range(1, 16):
@@ -70,84 +90,47 @@ for subject in range(1, 16):
         subject = str(subject)
         
     images_list = [x for x in file_list if re.search("^.*" + subject + ".*pgm$", x)]
+    images_list = [images_list[i] for i in indices]
 
-    for image_name in images_list[:split]:
+    for image_name in images_list:
         image_path = faces_folder_path + "\\" + image_name
         image = cv2.imread(image_path,-1)
-        #print(image_name)
-        target[target_index] = int(image_name[7:9])
-        target_index += 1
-        
+
         faces = detector(image, 1)
         for i, face in enumerate(faces):
             shape = predictor(image, face)
             shape = face_utils.shape_to_np(shape)
                 
         representative_landmark = [ sum([point[0] for point in shape]) / 68, sum([point[1] for point in shape]) / 68]
-        train_distances = [0] * 68
+        distances = [0] * 68
         for (name, (i,j)) in face_utils.FACIAL_LANDMARKS_IDXS.items():
             index = i
             for (x, y) in shape[i:j]:
-                train_distances[index] = euclidean_distance(representative_landmark, (x,y))
+                distances[index] = euclidean_distance(representative_landmark, (x,y))
                 index += 1
-        Train[z,:] = train_distances
-        z += 1
+        if images_list.index(image_name) < split:
+            Train[train,:] = distances
+            train_target[target_index] = int(image_name[7:9])
+            target_index += 1
+            train += 1
+        else:
+            Test[test,:] = distances
+            test_target[test_target_index] = int(image_name[7:9])
+            test_target_index += 1
+            test += 1
+            
    
-
-Train = np.append(Train, target, axis=1)
-#print(Train.shape)
+Train = np.append(Train, train_target, axis=1)
 
 trainset_df = pd.DataFrame(data=Train, index=[x for x in range(120)], columns=[x for x in range(69)])
-#print(trainset_df)
 X_train = trainset_df.iloc[:,list(range(0,68))]
 y_train = trainset_df.iloc[:, 68]
-clf = svm.SVC(kernel = "poly", C=1.0)
+
+clf = svm.SVC(kernel = "poly", C = 1.0)
 clf.fit(X_train, y_train)
 
-target = np.zeros((15 * (11 - split),1))
-target_index = 0
-Test=np.zeros((15 * (11-split),68)) # Matrix of distances
-z = 0
-
-count = 0
-img_count = 0
-for subject in range(1, 16):
-    
-    subject_landmarks = []
-    if subject < 10:
-        subject = "0" + str(subject)
-    else:
-        subject = str(subject)
-        
-    images_list = [x for x in file_list if re.search("^.*" + subject + ".*pgm$", x)]
-
-    for test_image_name in images_list[split:]:
-        img_count += 1
-        test_image = faces_folder_path + "\\" + test_image_name
-
-        #print(test_image_name)
-        target[target_index] = int(test_image_name[7:9])
-        target_index += 1
-        
-        test_image = cv2.imread(test_image,-1)
-        test_image_faces = detector(test_image, 1)
-        
-        for i, face in enumerate(test_image_faces):
-            shape = predictor(test_image, face)
-            shape = face_utils.shape_to_np(shape)
-                
-        representative_landmark = [ sum([point[0] for point in shape]) / 68, sum([point[1] for point in shape]) / 68]
-        test_distances = [0] * 68
-        for (name, (i,j)) in face_utils.FACIAL_LANDMARKS_IDXS.items():
-            index = i
-            for (x, y) in shape[i:j]:
-                test_distances[index] = euclidean_distance(representative_landmark, (x,y))
-                index += 1
-        Test[z,:] = test_distances
-        z += 1
-           
 testset_df = pd.DataFrame(data=Test, index=[x for x in range(45)], columns=[x for x in range(68)])
 y_pred = clf.predict(testset_df)
 
 from sklearn.metrics import accuracy_score
-print("Accuracy is ",accuracy_score(target, y_pred))
+print(accuracy_score(test_target, y_pred) * 100, "->", indices)
